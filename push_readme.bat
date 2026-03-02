4d2cfcbf50aaf08e40814e6b4dfb2ba7fb8b375d
kamranhussainaugment-svg <kamranhussainaugment-svg@users.noreply.github.com>
2026-03-02 17:17:31 +0500

    Add README and project files

---
+++ push_readme.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Add README and project files"
+git push -u origin main
---
+++ setup_git.bat
@@ -0,0 +1,3 @@
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/kamranhussainaugment-svg/leads.git
